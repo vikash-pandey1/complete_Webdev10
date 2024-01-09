f4102a6431cb959698f716bd47c00f235f356a7a
vikash-pandey1 <vikashpandey7082@gmail.com>
2024-01-09 22:21:36 +0530

    add ejs

--- EJS_DIR/index.js
+++ EJS_DIR/index.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const path = require("path")
 const app = express();
+
 const port = 3000;
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"))
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/ig/:username",(req,res)=>{
+    const followers = ["adam",'ram','shyam','hari'];
+    let {username} = req.params;
+    res.render("instagram.ejs",{username,followers})
+})
+app.get("/hello",(req,res)=>{
+    res.send("hello");
+})
+
+app.get("/rolldice",(req,res)=>{
+    let diceVal = Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs",{diceVal});
+})
 
 app.listen(port,()=>{
     console.log(`port is listening on ${port}`);
